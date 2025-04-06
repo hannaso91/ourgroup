54b4f91fbcfe0bbd26b3f15d6cee68cd660a127f
hannaso91 <hannaso@hiof.no>
2025-04-06 22:43:25 +0200

    fjernet div for å slippe konflikter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Layout from './components/Layout'
-import MemberCard from './components/MemberCard'
-import Home from './components/Home'
 import MemberLayout from './components/MemberLayout'
 
 
@@ -13,7 +11,6 @@ function App() {
     <>
       <Layout>
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path='member/:slugmember' element={<MemberLayout />} />
         </Routes>
       </Layout>
