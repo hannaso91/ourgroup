8698e8e640ff35f05ef9aeee5d1986b9f1fc992f
Jimmy Østby <142933101+jimmyostby@users.noreply.github.com>
2025-04-07 15:23:36 +0200

    Oppretter scss-fil.
    Opprettet og startet med stilsetting av medlemssidene.

--- src/components/MemberLayout.jsx
+++ src/components/MemberLayout.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { useState } from "react"
 import { useParams } from "react-router-dom"
 import { fetchMemberData } from "../sanity/memberServices"
+import "../styles/member.scss"
 
 export default function MemberLayout(){
     const [member, setMember] = useState()
@@ -29,13 +30,15 @@ export default function MemberLayout(){
         <>
             {member ? (
                 <>
-            <h2 key={member._id}>{member.name}</h2>
+            
             <article>
                 <img src={member.image?.asset?.url} alt="bilde av gruppemedlem"/>
+                <h2 key={member._id}>{member.name}</h2>
                 <p>{member.age}</p>
                 <p>{member.email}</p>
                 <p>{member.description}</p>
             </article>
+            
             </>
             ) : (
                 <p>Forsøker å finne medlem.</p>
