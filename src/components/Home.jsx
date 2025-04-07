94cc91be13058c09f50675f9fcdcfa788f716da3
odaju <142748592+odaju@users.noreply.github.com>
2025-04-07 14:27:16 +0200

    Lagt til styles mappe og laget scss filer til prosjektet

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ export default function Home({members}) {
         <section>
             <h1>Gruppemedlemmer</h1> {/*Denne må gjøres dynamisk basert på brukerinput*/}
             <MembersCard memberCard={members} /> 
+            <p>hei</p>
         </section>
     )
-    
 }
