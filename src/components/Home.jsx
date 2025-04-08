641e89ce14b88606f1f8670ffbb7e5d08b1fb6f7
hannaso91 <hannaso@hiof.no>
2025-04-08 16:23:46 +0200

    ryddet pittelitt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,8 @@ export default function Home({members, logg }) {
     return(
         <section>
             <h1>Gruppemedlemmer</h1> {/*Denne må gjøres dynamisk basert på brukerinput*/}
-            <MembersCard memberCard={members}/>
-            <Logg  logg={logg} /> 
+                <MembersCard memberCard={members}/>
+                <Logg  logg={logg} /> 
         </section>
     )
 }
