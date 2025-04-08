c498a2dbe81baa085b5ddc2068b84bd4e9ff15e2
hannaso91 <hannaso@hiof.no>
2025-04-08 14:19:38 +0200

    fjernet det Jimmy legger inn

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,11 @@
 import { Link } from "react-router-dom";
-import logo from '../pictures/DOMinationLogo.png' // fant ut av denne måten å gjøre det på her https://stackoverflow.com/questions/43823289/how-to-import-image-svg-png-in-a-react-component
 
 export default function Layout({children}) {
     return(
         <>
         <header>
             <nav>
-                <Link to="/"><img src={logo} alt="Vår logo"/></Link>
+                
                 <ul>
                     <li><Link to="/">Hjem</Link></li>
                     <li><Link to="member/jimmy-ostby">Jimmy</Link></li>
