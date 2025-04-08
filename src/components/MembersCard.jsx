373e0821125c4f0764af3ab2ca7624e8917cd61c
hannaso91 <hannaso@hiof.no>
2025-04-08 15:50:24 +0200

    nettsiden fungerer, må pusse på css

--- src/components/MembersCard.jsx
+++ src/components/MembersCard.jsx
@@ -1,22 +1,16 @@
 import { Link } from "react-router-dom";
 
 export default function MembersCard({ memberCard }) {
-    return (
-      <>
-        {memberCard?.map((member) => (
-          <article className="forsidegruppe" key={member._id}>
-            <img src={member.image.asset.url} alt="bilde av gruppemedlem"/>
-            <p id="fetnavn">{member.name}</p>
   return (
     <>
       {memberCard?.map((member) => (
-        <Link
-          key={member._id}
-          to={`/member/${member.slug}`}
-        >
+        <Link key={member._id} to={`/member/${member.slug}`}>
           <article className="member-card">
-            <img src={member.image?.asset?.url} alt={`Bilde av ${member.name}`} />
-            <p>{member.name}</p>
+            <img
+              src={member.image?.asset?.url}
+              alt={`Bilde av ${member.name}`}
+            />
+            <p id="fetnavn">{member.name}</p>
             <p>{member.email}</p>
           </article>
         </Link>
@@ -24,5 +18,6 @@ export default function MembersCard({ memberCard }) {
     </>
   );
 }
+
 
   
