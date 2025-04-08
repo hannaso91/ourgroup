0369f2a9afc754b36f271e2359652b62dd77bec2
odaju <142748592+odaju@users.noreply.github.com>
2025-04-08 13:42:17 +0200

    Responsivitet og overskrift
    endret på responsivitet og fjernet overskriften

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,6 @@ import "../styles/home.scss"
 export default function Home({members}) {
     return(
         <section>
-            <h1 id="overskrift">Gruppemedlemmer</h1> {/*Denne må gjøres dynamisk basert på brukerinput*/}
             <MembersCard memberCard={members} /> 
         </section>
     )
