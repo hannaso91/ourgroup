554468fc386882fbe92a651db5cf8cefff20a867
odaju <142748592+odaju@users.noreply.github.com>
2025-04-10 12:56:48 +0200

    lagrer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,14 @@ import Logg from "./Logg";
 import MembersCard from "./MembersCard";
 import "../styles/home.scss"
 
-
 export default function Home({members, logg }) {
     return(
+        <>
         <section>
             <h1>Gruppemedlemmer</h1> {/*Denne må gjøres dynamisk basert på brukerinput*/}
-                <MembersCard memberCard={members}/>
-                <Logg  logg={logg} /> 
+                <MembersCard memberCard={members}/> 
+                <Logg  logg={logg} />
         </section>
+    </>
     )
 }
