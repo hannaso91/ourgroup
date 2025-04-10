75a83fa08dad2e62ce787f1830c92bae3d3d73ca
hannaso91 <hannaso@hiof.no>
2025-04-10 13:51:53 +0200

    fikset dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ import Home from './components/Home'
 import { fetchMembersByCard } from './sanity/memberServices'
 import { useEffect, useState } from 'react'
 import { getAllLogs } from './sanity/logg'
+import './styles/darkmode.scss';
+
+// fikk tilbakemelding i oblig 2 om at dark mode ble hvit i teksten og umulig å lese, det kunne jo ikke skje igjen så vi søkte rundt for å løse det
+// Fant da denne siden her https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-color-scheme og brukte denne for å løse dette.
+// Husket også fra webutvikling at det ble nevnt important, søkte derfor på google og fant denne https://www.w3schools.com/css/css_important.asp
+// De to tilsammen løste dette problemet og teksten i dark mode blir nå svart
 
 function App() {
   
