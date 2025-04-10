b791d1b537f78980e487d034b2292f04e0886742
hannaso91 <hannaso@hiof.no>
2025-04-10 18:26:20 +0200

    fikset så alt er sentrert på mobil

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,8 @@ import MembersCard from "./MembersCard";
 export default function Home({members, logg }) {
     return(
         <>
-        <section>
-            <h1>Gruppemedlemmer</h1> {/*Denne må gjøres dynamisk basert på brukerinput*/}
+        <section className="containergruppe">
+            <h1>Gruppemedlemmer</h1> 
                 <MembersCard memberCard={members}/> 
                 <Logg  logg={logg} />
         </section>
